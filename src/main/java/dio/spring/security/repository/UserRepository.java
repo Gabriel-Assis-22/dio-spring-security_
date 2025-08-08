d9099798bc3114e5ac9cae7c917866a18a25e563
Gabrie Roberto <152230629+Gabriel-Assis-22@users.noreply.github.com>
2025-08-08 13:35:05 -0300

    commit

--- src/main/java/dio/spring/security/repository/UserRepository.java
+++ src/main/java/dio/spring/security/repository/UserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     @Query("SELECT e FROM User e JOIN FETCH e.roles WHERE e.username=(:username)")
-    public User findByUsername(@Param("username") String username); 
+    public User findByUsername(@Param("username") String username);
 }
